087b494581997fc098a8cbece48d9e2f58a1437b
Teejayskillz <Teejayadewale87@gmail.com>
2025-09-26 00:46:47 +0100

    added django q tasks

--- core/signals.py
+++ core/signals.py
@@ -11,7 +11,6 @@ import asyncio
 import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django_q.tasks import async_task
 from .models import Post
 
 
@@ -118,7 +117,13 @@ def auto_post_to_telegram(sender, instance, **kwargs):
     else:
         logger.info(f"Post '{instance.title}' not published, skipping Telegram post.")
 
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django_q.tasks import async_task
+from .models import Post
+
 @receiver(post_save, sender=Post)
-def trigger_new_post_notification(sender, instance, created, **kwargs):
-    if created:  # Only run for a new post
-        async_task('core.tasks.send_new_post_notification', instance.id)
+def trigger_new_post_notification(sender, instance, created, **_kwargs):
+    if created:  
+        async_task('blog.tasks.send_new_post_notification', instance.id)
